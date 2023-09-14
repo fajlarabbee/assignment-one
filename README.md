9f84eeb0a9b8dcaed7c4cc9b752dd4bb9a2e6bb2
Fajla Rabbee <iamrabbee@gmail.com>
2023-09-14 11:58:05 +0600

    Personal Info task added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# assignment-one
+# Assignment One
+This repository contains the module one assignment solutions.
+
+### Task 1: Personal Information Page
+- This page will show only certain information about myself with a little description. It uses PHP's echo statement to print the information.
---
+++ personal_info.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Task 1: Personal Information Page
+ * Create a PHP file named personal_info.php that displays your personal information using variables and the echo statement. Include your name, age, country, and a brief introduction.
+ */
+
+$name = "Fajla Rabbee";
+$age = 25;
+$country = "Bangladesh";
+$sector = "Web Development";
+echo "<h3>Hello there, I am {$name}, a {$age}-year-old {$sector} enthusiast based in {$country}, is trying to learn and improve my skills in {$sector} sector with a passion for web technologies.</h3>";
